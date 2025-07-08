80810d37908779031a5aee169eec37f741e5f454
Vlad Manea <vladmanea@Vlads-MacBook-Air.local>
2025-07-08 21:21:09 +0100

    Add calculator logic

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-        boolean calculate = true;
-        while (calculate) {
-            System.out.println("What is your first number: ");
-            double firstNumber = sc.nextDouble();
-            System.out.println("What is your second number: ");
-            double secondNumber = sc.nextDouble();
-            System.out.println("What operand would you like to use: ");
-            double operand = sc.nextDouble();
+    boolean calculate = true;
+    while (calculate) {
+      System.out.println("What is your first number: ");
+      double firstNumber = sc.nextDouble();
+      System.out.println("What is your second number: ");
+      double secondNumber = sc.nextDouble();
+      System.out.println("What operand would you like to use: ");
+      String operand = sc.next();
 
-            double sumResult = firstNumber + secondNumber;
-            System.out.println("Sum: " + sumResult);
+//      if (operand.equals("+")) {
+//        double sum = firstNumber + secondNumber;
+//        System.out.println("sum: " + sum);
+//      } else if (operand.equals("-")) {
+//        double subtraction = firstNumber - secondNumber;
+//        System.out.println("subtraction: " + subtraction);
+//      } else if (operand.equals("/")) {
+//        double division = firstNumber / secondNumber;
+//        System.out.println("division: " + division);
+//      } else if (operand.equals("*")) {
+//        double multiplication = firstNumber * secondNumber;
+//        System.out.println("multiplication: " + multiplication);
+//      }
 
-            double product = firstNumber * secondNumber;
-            System.out.println("Product: " + product);
+      switch (operand) {
+        case ("+"):
+          double sum = firstNumber + secondNumber;
+          System.out.println("sum: " + sum);
+          break;
+        case ("-"):
+          double subtraction = firstNumber - secondNumber;
+          System.out.println("subtraction: " + subtraction);
+          break;
+        default:
+          double multiplication = firstNumber * secondNumber;
+          System.out.println("multiplication: " + multiplication);
+          break;
+      }
 
-            double division = firstNumber / secondNumber;
-            System.out.println("Division: " + division);
-
-            double subtraction = firstNumber - secondNumber;
-            System.out.println("Subtraction: " + subtraction);
-
-            System.out.println("Would you like to continue: ");
-        }
-        boolean stopCalculating = false;
-        if (stopCalculating) {
-            System.out.println("Finished calculating session");
-        }
-
-        sc.close();
+      System.out.println("Would you like to continue (True or False): ");
+      calculate = sc.nextBoolean();
     }
+
+    sc.close();
+  }
 }
